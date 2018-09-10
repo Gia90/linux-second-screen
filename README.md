740eb4fd08b27e9155fc4dc464f700752fb566d2
Gia90 <Gia90@users.noreply.github.com>
2018-09-10 17:27:55 +0200

    Lot of changes to automate what possible and consider the android devcie real dimension (DPI) [WIP]

--- README.md
+++ README.md
@@ -1,17 +1,6 @@
 # linux-second-screen
 App to use any android device as second monitor on linux.
 
-## How it will work
-The app may have multiples MainActivities.   
-1. [ ] Push the app using adb   
-3. [x] Ask device for it's resolution. (using `adb` or the `app`)   
-4. [x] Create a virtual display matching device answer   
-5. [x] Start vnc server   
-6. [ ] Push host's configurations to device   
-7. [ ] Start the app using adb   
-We already made some of those things using a simple script.   
-
-
 # The power of the script
 Take a look at the [monitor.sh](https://github.com/Dlimaun/linux-second-screen/blob/master/monitor.sh).   
 This script will create the virtual monitor and start x11vnc server for you.   
@@ -25,19 +14,10 @@ You only need to pass those parameters, if you want. The parameters could be pas
 ## Mixing with ADB?
 Some Android device return can return the resolution throught command line.   
 To use that you will need to enable developer mode on your device. [Check this](http://developer.android.com/tools/help/adb.html#Enabling) to enable.   
-To finish you'll need to point your adb location inside the script file.   
 With this you don't need to pass any parameters to the script.   
-(This is only recomended for developers)
-
-
-## For those who aren't using a laptop.
-In script file, there's a comment on top asking you to select one of those `fisical`.   
-So if you are using `VGA` or `HDMI` video output, comment `fisical="LVDS1"` and uncomment your video output.   
-
 
 ## if you still want to do it by hand
 Follow this [tutorial](https://github.com/Dlimaun/linux-second-screen/blob/master/tutorial.md).   
-
 
 # Similar apps
 ~~This app will be made by the rage because we have a lot of apps for all other platforms
@@ -48,7 +28,3 @@ Only linux isn't supported by those apps.~~
 * [Splashtop Extended Display Android](https://play.google.com/store/apps/details?id=com.splashtop.remote.xdisplay)
 * [iDisplay Android](https://play.google.com/store/apps/details?id=com.idisplay.virtualscreen)
 * [DuetDisplay](http://www.duetdisplay.com/)
-
-
-Splashtop appear to have a beta, for linux.   
-\o/
--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# DEBUG
+#set -x
+
 ## PARAMS
 ## Device resolution using [width]x[height], without bracets. Sample 800x600
 ## -v - VIRTUAL display to be used. Sample v1, v2, v3
@@ -8,16 +11,12 @@
 ## -hst	  - Subtract status bar size from virtual display
 ## -hsb	  - Subtract system bar size from virtual display
 
-### Uncomment what you are using, hardcoded
-## Laptop
-fisical="LVDS1"
-## VGA
-#fisical="VGA1"
-## HDMI
-#fisical="HDM1"
+## Primary physical screen name
+physical=$(xrandr | grep primary | awk '{print $1}')
 
-## ADB path. hardcoded
-adb_bin=~/android-sdk-linux/platform-tools/adb
+## ADB path
+# TODO: require adb in the path?
+adb=$(which adb 2>/dev/null || echo "~/android-sdk-linux/platform-tools/adb")
 
 #echo $@
 ## Regex to understand params
@@ -36,24 +35,96 @@ else
 	virtual="VIRTUAL${virtual}"
 fi
 
-## Find Android device Resolution, some devices works
+diagonal() {
+  printf '%.0f' $(bc <<< "scale=2; sqrt($1^2+$2^2)")
+}
+
+
+
+#******** HOST ********
+
+## Find host resolution
+h_res=$(xrandr | grep \* | awk '{print $1}')
+h_width=$(echo $h_res | cut -d'x' -f 1)
+h_height=$(echo $h_res | cut -d'x' -f 2)
+h_diag=$(diagonal $h_width $h_height)
+echo "h_diag = $h_diag"
+
+## Calculate host dpi (XORG reports wrong dpi, since it always defualts to 96 dpi)
+#h_dpi=$(xdpyinfo | grep resolution | awk '{print $2}' | cut -d"x" -f1)
+h_psize=$(xrandr | grep eDP | awk '{print $(NF-2), $(NF)}' | sed "s/mm//g")
+h_pwidth=$(echo "$h_psize" | cut -d' ' -f1)
+h_pheight=$(echo "$h_psize" | cut -d' ' -f2)
+h_pdiag=$(bc <<< "scale=0; $(diagonal $h_pwidth $h_pheight)/25.4")
+h_dpi=$(bc <<< "scale=2; $h_diag/$h_pdiag" | xargs printf '%.0f')
+
+echo "HOST"
+echo "h_res    = $h_res"
+echo "h_width   = $h_width"
+echo "h_height  = $h_height"
+echo "h_psize = $h_psize"
+echo "h_pwidth = $h_pwidth"
+echo "h_pheight = $h_pheight"
+echo "h_pdiag = $h_pdiag"
+echo "h_dpi = $h_dpi"
+
+
+
+#******** DEVICE ********
+
+## Find Android device resolution with the current orientation
 if [ -z "$device" ] ; then
-	device=$($adb_bin shell dumpsys window displays | grep init | cut -d'=' -f 2 | cut -d' ' -f 1)
+  device=$($adb shell dumpsys window displays | grep init | awk '{print $3}' | cut -d'=' -f2)
 fi
 if [ -z "$device" ] ; then
 	echo "Can't read device resolution using adb"
-	exit 0
+	exit 1
 else
 	## Device width and height
 	d_width=$(echo $device | cut -d'x' -f 1)
 	d_height=$(echo $device | cut -d'x' -f 2)
 fi
 
+# TODO: keep windows size looking the same on the virtual monitor.... HOW? --> DPI+SCALE
+#d_dpi=$(adb shell wm density | cut -d' ' -f 3)  # Slower and working only on newer android versions
+d_dpi=$(adb shell dumpsys display | grep DisplayDeviceInfo  | sed -n -e 's/^.*density \([0-9]\+\).*/\1/p')
+d_orientation=$(adb shell dumpsys input | grep 'SurfaceOrientation' | awk '{ print $2 }')
+
+echo
+echo
+echo "DEVICE"
+echo "d_width = $d_width"
+echo "d_height = $d_height"
+echo "d_dpi = $d_dpi"
+echo "d_orientation = $d_orientation"
+
+
+
+#******** VIRTUAL ********
+
+# TODO: proportion needed?
+## Proportion, bash don't handle float, only integers so we use bc to do that operation
+proportion=$(bc <<< "scale=2; $d_height / $h_height")
+v_width=$(bc <<< "scale=0; $d_width / $proportion")
+v_height=$h_height
+#v_scale=$(bc <<< "scale=2; 1/$proportion" | awk '{printf "%f", $0}')  # TODO: we need to take the resoultion difference in account for the scale. For now, we ignore this (same res)
+
+# TODO: STATUS BAR - NAVIGATION BAR 
+status_bar=32
+system_bar=48
+## Remove status bar height
+if [ ! -z "$hide_statusbar" ] ; then
+	v_height=$(($v_height - $status_bar))
+fi
+## Remove navigation bar height
+if [ ! -z "$hide_systembar" ] ; then
+	v_height=$(($v_height - $system_bar))
+fi
+
 ## Check param position, this position is where the user want the new screen
 if [ -z "$position" ] ; then
-	position="left"
+	position="right"
 fi
-#echo "position= $position"
 if [ "$position" = "left" ] ; then
 	xinerama="xinerama0"
 else
@@ -61,90 +132,38 @@ else
 fi
 
 
+# Add traling zero to bc output
+v_scale=$(bc <<< "scale=2; $h_dpi / $d_dpi" | awk '{printf "%f", $0}')
+v_width=$(bc <<< "$d_width*$v_scale")
+v_height=$(bc <<< "$d_height*$v_scale")
 
-## Find Host Resolution
-host=$(xdpyinfo  | grep 'dimensions:' | cut -d' ' -f 7)
-h_width=$(echo $host | cut -d'x' -f 1)
-h_height=$(echo $host | cut -d'x' -f 2)
-
-
-
-## Find Possible IPs
-echo "Possible IPs, use 'ifconfig' to check it out, if you want"
-ifconfig | grep 'inet addr' | cut -d':' -f 2 | cut -d' ' -f 1
-echo ""
-
-
-
-
-## Proportion, bash don't handle float, only integers so we use bc to do that operation
-##proportion=$(($d_height / $h_height))
-proportion=$(bc <<< "scale=2; $d_height / $h_height")
-v_width=$(bc <<< "scale=0; $d_width / $proportion")
-v_height=$h_height
-#echo "width   = $v_width"
-status_bar=32
-system_bar=48
-## Remove status bar height
-if [ ! -z "$hide_statusbar" ] ; then
-	v_height=$(($v_height - $status_bar))
-fi
-## Remove system bar height
-if [ ! -z "$hide_systembar" ] ; then
-	v_height=$(($v_height - $system_bar))
-fi
-#echo "height  = $v_height"
+v_width=$d_width
+v_height=$d_height
 
 
 ## Build the modeline, the display configurations
-modeline=$(cvt $v_width $v_height 20.00 | grep "Modeline" | cut -d' ' -f 2-17)
-## Find the mode
+modeline=$(cvt $v_width $v_height 60.00 | grep "Modeline" | cut -d' ' -f 2-17)
 mode=$(echo "$modeline" | cut -d' ' -f 1)
-## remove quotes, don't need to remove quotes
-#mode=${mode//\"}
-res=$(echo ${mode//\"} | cut -d'_' -f 1)
 
-
-## Evaluates the start width position, to clip vnc
-#s_width=$(echo $host | cut -d'x' -f 1)
-#s_width=$((s_width + 1))
-#echo "s_width = $s_width"
-
-#echo $modeline
-#echo $mode
-
-echo "device  = $device"
-#echo "width   = $d_width"
-#echo "height  = $d_height"
-
-echo "host    = $host"
-#echo "width   = $h_width"
-#echo "height  = $h_height"
-echo "scale   = $proportion"
-
+echo
 echo "Display = $virtual"
-
-echo "virtual = $res"
-echo ""
-
-## Log and Run commands
-function run () {
-	echo "$1"
-	$1
-}
+echo "v_scale   = $v_scale"
+echo "mode = $mode"
+echo "modeline = $modeline"
 
 ## Create Virtual Display
-run "xrandr --newmode $modeline"
-run "xrandr --addmode $virtual $mode"
-run "xrandr --output $virtual --mode $mode --${position}-of ${fisical}"
-
+xrandr --newmode $modeline
+xrandr --addmode $virtual $mode
+xrandr --output $virtual --mode $mode --${position}-of ${physical} --scale ${v_scale}x${v_scale}
 
 ## Start VNC
-run "x11vnc -clip ${xinerama} -xrandr -ncache 1 -nosel -viewonly -fixscreen \"V=2\" -noprimary -nosetclipboard -noclipboard -cursor arrow -nopw -nowf -nonap -noxdamage -sb 0 -display :0"
-
+x11vnc --auth guess -rfbauth /home/$USER/.vnc/passwd -once \
+  -clip ${xinerama} -xrandr -nosel -viewonly -fixscreen \"V=2\" -noprimary -nosetclipboard -noclipboard -cursor most -nopw -nowf -nonap -noxdamage -sb 0 \
+  -display :0
 
 ## Turn VirtualDisplay off
-run "xrandr --output $virtual --off"
-run "xrandr --delmode $virtual $mode"
-run "xrandr --rmmode $mode"
-run "xrandr -s 0"
+xrandr --output $virtual --off
+xrandr --delmode $virtual $mode
+xrandr --rmmode $mode
+# TODO: set size to 0.. is it needed?
+#xrandr -s :0
